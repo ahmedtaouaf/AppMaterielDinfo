c6722922c479498042447076664c8776d500e457
hamza-boulila <taouafhero@gmail.com>
2024-11-20 13:03:23 +0100

    schemas page fixed

--- src/main/java/com/app/materiel/Controllers/ServeurController.java
+++ src/main/java/com/app/materiel/Controllers/ServeurController.java
@@ -1,14 +1,12 @@
 package com.app.materiel.Controllers;
 
 import com.app.materiel.Entity.Serveur;
-import com.app.materiel.Entity.Stock;
 import com.app.materiel.Entity.VirtualMachine;
 import com.app.materiel.Repository.VirtualMachineRepository;
 import com.app.materiel.Service.ServeurService;
 import com.app.materiel.Service.VirtualMachineService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ServeurController {
@@ -107,6 +104,27 @@ public class ServeurController {
 
         redirectAttributes.addFlashAttribute("deleteMessage", "Serveur Supprimé Avec Succés !");
         return "redirect:/serveurs/"+serveurResau;
+    }
+
+    @Value("${materiel.schemas}")
+    String svgDirectoryPath;
+
+    @GetMapping("/schema/liste")
+    public String getSvgCatalogue(Model model) {
+
+        //String svgDirectoryPath = "C:\\svgs";
+        File folder = new File(svgDirectoryPath);
+        File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".svg"));
+
+        List<String> svgPaths = new ArrayList<>();
+        if (files != null) {
+            for (File file : files) {
+                svgPaths.add("/svgs/" + file.getName());
+            }
+        }
+
+        model.addAttribute("svgPaths", svgPaths);
+        return "schemasresaux";
     }
 
 
