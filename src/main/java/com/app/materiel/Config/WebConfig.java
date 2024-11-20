50f85182ad57ae47ce91c204414a9c931debd587
hamza-boulila <taouafhero@gmail.com>
2024-11-20 14:26:20 +0100

    local variable added

--- src/main/java/com/app/materiel/Config/WebConfig.java
+++ src/main/java/com/app/materiel/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.app.materiel.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${handler.file}")
+    String svgHandlerPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+
+
         registry.addResourceHandler("/svgs/**")
-                .addResourceLocations("file:C:/svgs/");
+                .addResourceLocations(svgHandlerPath);
     }
 }
